cd54d6109157678c9e1c312faa0b01e5a33677a4
nazarenofigoli <nazarenofigoli69@gmail.com>
2023-11-06 14:53:18 -0300

    inicio de estructura del carrito

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
 });
 
+app.get("/carrito", (req, res)=>{
+    res.sendFile(path.join(__dirname,"/views/carrito.html"))
+});
+
 app.use(express.static("public"));
 
 app.listen(8080,()=>{
