750eca888dfe33cfbe4ef016aef72bfad1755c0b
CastilloValeria <casvaleli@gmail.com>
2023-11-06 17:43:22 -0300

    cree archivo detalleP.html, en app agregue la get para servidor y en img ilustracciones

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require ("express");
 const app = express();
 const path = require("path");
 
+// app.get("/", (req, res)=>{
+//     res.sendFile(path.join(__dirname,"/views/index.html"))
+// });
+
+
 app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname,"/views/index.html"))
-});
+    res.sendFile(path.join(__dirname,"/views/detalleP.html"))
+}); 
 
 app.use(express.static("public"));
 
