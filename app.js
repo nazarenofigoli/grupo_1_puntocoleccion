cd3e4ed1fb32e70c8370fd956d52828fff4843d3
CastilloValeria <casvaleli@gmail.com>
2023-11-18 01:20:17 -0300

    cambios en mi rama

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
 });
 
-<<<<<<< HEAD
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/detalle", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/detalleP.html"))
 }); 
-=======
+
 app.get("/registro", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/registro.html"))
 });
@@ -23,7 +34,7 @@ app.post ('/registro',(req,res)=>{
     console.log(req.body);
     res.redirect("/");
     })
->>>>>>> origin/EC-47_Registro
+
 
 app.use(express.static("public"));
 
@@ -31,9 +42,6 @@ app.listen(8080,()=>{
     console.log("Puerto 8080 iniciado")
 });
 
-<<<<<<< HEAD
 app.get("/header", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/header.html"))
 });
-=======
->>>>>>> origin/EC-47_Registro
