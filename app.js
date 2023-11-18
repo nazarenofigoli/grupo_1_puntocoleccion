5a1442cdb6fc1cc94488b258dafd70ed5a10ff09
CastilloValeria <casvaleli@gmail.com>
2023-11-18 01:03:42 -0300

    mergee mi rama para guardar cambios con la rama registro

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require ("express");
 const app = express();
 const path = require("path");
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
 });
 
+<<<<<<< HEAD
 
 app.get("/detalle", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/detalleP.html"))
 }); 
+=======
+app.get("/registro", (req, res)=>{
+    res.sendFile(path.join(__dirname,"/views/registro.html"))
+});
+
+app.post ('/registro',(req,res)=>{
+    console.log(req.body);
+    res.redirect("/");
+    })
+>>>>>>> origin/EC-47_Registro
 
 app.use(express.static("public"));
 
@@ -17,6 +31,9 @@ app.listen(8080,()=>{
     console.log("Puerto 8080 iniciado")
 });
 
+<<<<<<< HEAD
 app.get("/header", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/header.html"))
 });
+=======
+>>>>>>> origin/EC-47_Registro
