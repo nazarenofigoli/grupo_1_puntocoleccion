0b6c31356e845806392ba0db4b59205cd68ac65b
CastilloValeria <casvaleli@gmail.com>
2023-11-19 02:04:10 -0300

    agregando max width a las media queries

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get("/detalle", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/detalleP.html"))
 }); 
 
+app.post ('/detalle',(req,res)=>{
+    console.log(req.body);
+    res.redirect("/");
+    })
+
 app.get("/registro", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/registro.html"))
 });
@@ -22,10 +27,17 @@ app.post ('/registro',(req,res)=>{
     res.redirect("/");
     })
 
+    app.get("/login", (req, res)=>{
+        res.sendFile(path.join(__dirname,"/views/login.html"))
+    });
 
 app.get("/carrito", (req, res)=>{
     res.sendFile(path.join(__dirname,"/views/carrito.html"))
 });
+app.get("/header", (req, res)=>{
+    res.sendFile(path.join(__dirname,"/views/header.html"))
+});
+
 
 app.use(express.static("public"));
 
@@ -33,6 +45,4 @@ app.listen(8080,()=>{
     console.log("Puerto 8080 iniciado")
 });
 
-app.get("/login", (req, res)=>{
-    res.sendFile(path.join(__dirname,"/views/login.html"))
-});
+
