3a17df059b417ecd3e643278d4a3cca7b3ff450e
nazarenofigoli <nazarenofigoli69@gmail.com>
2023-12-03 15:11:58 -0300

    Agregue metodo post para registro y login

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const fs = require('fs');
 
 const userControllers = {
     login: (req,res)=>  res.render('login', {title:'Login'}),
-    registro: (req,res)=>  res.render('registro', {title:'Registro'})
+    registro: (req,res)=>  res.render('registro', {title:'Registro'}),
 
+    createUsers:(req,res)=> {
+        console.log(req.body);
+        res.redirect('/')
+    },
+    
+    createLogueo:(req,res)=> {
+        console.log(req.body);
+        res.redirect('/')
+    }
 }
 
 
