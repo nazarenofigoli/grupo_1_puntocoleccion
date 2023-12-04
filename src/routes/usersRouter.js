8f14f806c4e42438a8690dfaaece9ed94a336921
nazarenofigoli <nazarenofigoli69@gmail.com>
2023-12-04 15:08:21 -0300

    Cree metodo post para el login

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,7 +5,7 @@ const userControllers = require('../controllers/usercontrollers');
 router.get('/login', userControllers.login);
 router.get('/registro', userControllers.registro);
 router.post('/registro', userControllers.createUsers);
-router.post('/registro', userControllers.createLogueo);
+router.post('/login', userControllers.createLogueo);
 
 
 
