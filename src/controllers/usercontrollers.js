2aaa78ef06fe121965e268ad4411df2d13e4fc32
Lucioo04 <luciotesta@gmail.com>
2023-12-05 09:23:45 -0300

    add

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const userControllers = {
-    login: (req,res)=>  res.render('login', {title:'Login'}),
+    login: (req,res)=>  res.render( 'login',{title:'Login'}),
     registro: (req,res)=>  res.render('registro', {title:'Registro'})
 
 }
