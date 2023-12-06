6779656009e3801378b446871633809ab6e6e2e1
PaulaEscobar <aanitaescobar@gmail.com>
2023-12-06 19:08:24 -0300

    odificando formulario de carga

--- src/controllers/productcontrollers.js
+++ src/controllers/productcontrollers.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
+
 const productControllers = {
     detalleP: (req,res)=>  res.render('detalleP', {title:''}),
-    carrito: (req,res)=>  res.render('carrito', {title:'Carrito'})
+    carrito: (req,res)=>  res.render('carrito', {title:'Carrito'}),
+    cargaDeProducto:  (req,res)=>  res.render('cargaDeProducto', {title:'cargaDeProducto'}),
 
 }
 
