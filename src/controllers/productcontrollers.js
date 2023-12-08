166efbfa6b909dc4b9200700aae5cb804cefd668
PaulaEscobar <aanitaescobar@gmail.com>
2023-12-08 13:30:44 -0300

    arreglando ruta

--- src/controllers/productcontrollers.js
+++ src/controllers/productcontrollers.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const productControllers = {
     detalleP: (req,res)=>  res.render('detalleP', {title:''}),
-    carrito: (req,res)=>  res.render('carrito', {title:'Carrito'}),
-    cargaDeProducto:  (req,res)=>  res.render('cargaDeProducto', {title:'cargaDeProducto'}),
+    carrito: (req,res)=>  res.render('product/carrito', {title:'Carrito'}),
+    cargaDeProducto:  (req,res)=>  res.render('product/cargaDeProducto', {title:'cargaDeProducto'}),
 
 }
 
