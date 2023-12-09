d47ec23b82d72da041c43f41b38aa7337085d116
Lucioo04 <luciotesta@gmail.com>
2023-12-08 21:54:19 -0300

    Se creo el formulario para editar el archivo y su respectiva ruta y controlador para ser rendizado

--- src/controllers/productcontrollers.js
+++ src/controllers/productcontrollers.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 const productControllers = {
     detalleP: (req,res)=>  res.render('detalleP', {title:''}),
-    carrito: (req,res)=>  res.render('carrito', {title:'Carrito'})
+    carrito: (req,res)=>  res.render('carrito', {title:'Carrito'}),
+    update: (req,res)=> res.render ('products/updateproduct',{title:'Editar Producto'})
 
 }
 
