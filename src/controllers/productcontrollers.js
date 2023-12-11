9a5b6c9ab70934b6f4088a233ad820001c61d2d4
CastilloValeria <casvaleli@gmail.com>
2023-12-11 07:01:20 -0300

    corrigiendoredireccionado en dashboar y refactorizando carpetas que estaban duplicadas producto del merge, revie en el ararchivo controller las rytas para que estos cambios realizados no alteren el correcto funcionamiento

--- src/controllers/productcontrollers.js
+++ src/controllers/productcontrollers.js
@@ -5,10 +5,10 @@ const products = JSON.parse(json);
 
 
 const productControllers = {
-    detalleP: (req,res)=>  res.render('detalleP', {title:''}),
-    carrito: (req,res)=>  res.render('product/carrito', {title:'Carrito'}),
+    detalleP: (req,res)=>  res.render('products/detalleP', {title:''}),
+    carrito: (req,res)=>  res.render('products/carrito', {title:'Carrito'}),
     update: (req,res)=> res.render ('products/updateproduct',{title:'Editar Producto'}),
-    cargaDeProducto:  (req,res)=>  res.render('product/cargaDeProducto', {title:'cargaDeProducto'}),
+    cargaDeProducto:  (req,res)=>  res.render('products/cargaDeProducto', {title:'cargaDeProducto'}),
 
     dashboard:(req, res) => {
         res.render('products/dashboard',{title:"Dashboard",products});
