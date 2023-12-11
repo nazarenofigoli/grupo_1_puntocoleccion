118e08d36dcf2b900dae8e0609b6de633eb83006
CastilloValeria <casvaleli@gmail.com>
2023-12-11 07:33:50 -0300

    modificando redireccionado en dashboard por refactorizacion de carpetas luego de hacer pull en rama develop

--- src/controllers/productcontrollers.js
+++ src/controllers/productcontrollers.js
@@ -5,7 +5,7 @@ const products = JSON.parse(json);
 
 
 const productControllers = {
-    detalleP: (req,res)=>  res.render('products/detalleP', {title:''}),
+    detalleP: (req,res)=>  res.render('products/detalleP', {title:'Detalle de producto'}),
     carrito: (req,res)=>  res.render('products/carrito', {title:'Carrito'}),
     update: (req,res)=> res.render ('products/updateproduct',{title:'Editar Producto'}),
     cargaDeProducto:  (req,res)=>  res.render('products/cargaDeProducto', {title:'cargaDeProducto'}),
