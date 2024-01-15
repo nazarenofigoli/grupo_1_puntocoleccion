c722d90a13f96c8dfd1146866dedf353e9af3302
nazarenofigoli <nazarenofigoli69@gmail.com>
2024-01-15 09:15:29 -0300

    Avanzando en tarea de validar formulario de  login

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -1,18 +1,23 @@
 const path = require('path');
 const fs = require('fs');
+const { validationResult } = require('express-validator');
+
 
 const userControllers = {
     login: (req,res)=>  res.render('./users/login', {title:'Login'}),
     registro: (req,res)=>  res.render('./users/registro', {title:'Registro'}),
 
-    createUsers:(req,res)=> {
-        console.log(req.body);
-        res.redirect('/users/login')
-    },
     
+    createUsers:(req,res)=> {
+        let errors = validationResult(req);
+        console.log(req.body);
+        res.send({ errors: errors.mapped()})
+
+    },
+
     createLogueo:(req,res)=> {
         console.log(req.body);
-        res.send('/users/registro')
+        res.redirect('/')
     }
 }
 
--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -1,28 +1,31 @@
 const { body} = require('express-validator');
 const bcrypt = require('bcrypt');
 
-module.exports = [
-    body('email')
-        .notEmpty().withMessage("El campo no puede estar vacío").bail()
-        .isEmail().withMessage("El valor ingresado debe tener el formato de un correo electrónico").bail()
-        .custom(value => {
-            console.log("value:", value);
-            const user = users.find(elemento => elemento.email == value);
-            if (!user) {
-                throw new Error("El usuario no existe");
-            }
-            return true;
-        }),
-    body('password')
-        .notEmpty().withMessage("El campo no puede estar vacío").bail()
-        .custom((value, { req }) => {
-            console.log("password:", value);
-            const user = users.find(elemento => elemento.email == req.body.email);
-            console.log("user:", user);
 
-            if (!user || !bcrypt.compareSync(value, user.password)) {
-                throw new Error("La contraseña no es correcta");
-            }
-            return true;
-        })
+const validateLogin = [
+    body('email')
+    .notEmpty().withMessage("El campo no puede estar vacío").bail()
+    .isEmail().withMessage("El valor ingresado debe tener el formato de un correo electrónico").bail()
+    .custom(value => {
+        console.log("value:", value);
+        const user = users.find(elemento => elemento.email == value);
+        if (!user) {
+            throw new Error("El usuario no existe");
+        }
+        return true;
+    }),
+body('password')
+    .notEmpty().withMessage("El campo no puede estar vacío").bail()
+    .custom((value, { req }) => {
+        console.log("password:", value);
+        const user = users.find(elemento => elemento.email == req.body.email);
+        console.log("user:", user);
+
+        if (!user || !bcrypt.compareSync(value, user.password)) {
+            throw new Error("La contraseña no es correcta");
+        }
+        return true;
+    })
 ];
+
+module.exports = validateLogin;
