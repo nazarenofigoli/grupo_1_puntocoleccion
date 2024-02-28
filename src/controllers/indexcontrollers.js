ec39d9c98faf9b42862c1de9055c5ac2d40a0249
Lucioo04 <luciotesta@gmail.com>
2024-02-28 20:11:08 -0300

    refactorizando vista principal (index)

--- src/controllers/indexcontrollers.js
+++ src/controllers/indexcontrollers.js
@@ -1,25 +1,28 @@
 const fs = require("fs");
 const path = require("path");
-const json = fs.readFileSync(path.join(__dirname,"../database/product.json"),"utf-8")
-const products = JSON.parse(json);
+
 const db = require('../database/models')
 
 
 const indexControllers = {
   index: (req, res) => {
-    db.Product.findOne ( { where : {subcategoria_id: 1}}) 
-        .then ((result) => {tendenciasYOfetas= result} )
-    db.Product.findOne ( { where : {subcategoria_id: 2}}) 
-        .then ((result) => {destacados= result} )    
-    
-    res.render('index', {title: 'Punto Coleccion',tendenciasYOfetas, destacados , usuario:req.session.user});
-  }
-
-
-
-  
-};
-
-
+    db.Product.findAll({
+      where: { subcategoria_id: 1 },
+      include: [
+        {model: db.Imageproduct,
+        limit: 2}]})
+      .then ((result) => {tendenciasYOfertas = result})
+    db.Product.findAll({ 
+      where: { subcategoria_id: 2 },
+      include: [{
+        model: db.Imageproduct,
+        limit: 1}]})
+        .then ((result) => {destacados=result;
+        res.render('index', {title: 'Punto Coleccion',tendenciasYOfertas,destacados, usuario: req.session.user}) 
+      
+    })
+    .catch((err) => console.log(err))
+}
+}
 module.exports = indexControllers;
 
