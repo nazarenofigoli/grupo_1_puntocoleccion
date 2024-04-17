7cf1ee10a2b738ffdd987d808c3d5e49a82433cc
nazarenofigoli <nazarenofigoli69@gmail.com>
2024-04-17 20:23:51 -0300

    Agrego formato componente dashboard

--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -7,26 +7,38 @@ function NavBar() {
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          
-<Navbar.Brand href="http://localhost:3000/">
-<img
-  src="./LogoP2.png"
-  width="60"
-  height="60"
-  className="d-inline-block align-top"
-  alt="React Bootstrap logo"
-/></Navbar.Brand>
+          <Navbar.Brand href="http://localhost:3000/" target='_blank'>
+            <img 
+              src="./LogoP2.png"
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link 
+              href="http://localhost:3000/users/all" 
+              style={{ fontSize: '1.2em', color: 'white' }}
+              onMouseOver={(e) => e.target.style.color = 'green'}
+              onMouseOut={(e) => e.target.style.color = 'white'}>
+              Usuarios
+            </Nav.Link>
+            <br></br>
+            <br></br>
+            <Nav.Link 
+              href="http://localhost:3000/products/dashboard" 
+              style={{ fontSize: '1.2em', color: 'white' }}
+              onMouseOver={(e) => e.target.style.color = 'green'}
+              onMouseOut={(e) => e.target.style.color = 'white'}>
+              Productos
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
     </>
   );
 }
-
 
 
 export default NavBar;
